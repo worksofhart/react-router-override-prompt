16c60fde2cabb1db6a7aaf49b66b9702667083b6
Michael Hart <michael@mhart.dev>
2019-12-27 12:59:01 -0500

    RescueForm converted to use useEffect

--- src/components/RescueForm.js
+++ src/components/RescueForm.js
@@ -1,18 +1,22 @@
+import { useEffect } from "react";
 import axios from "axios";
 
 const RescueForm = ({ message, callback }) => {
-  axios
-    .post("https://webhook.site/d2f59c44-4b9a-4f53-a2a6-16ae50bd83c4", {
-      foo: "foo"
-    })
-    .then(res => {
-      console.log(res);
-      callback(true);
-    })
-    .catch(err => {
-      console.log(err);
-      callback(true);
-    });
+  useEffect(() => {
+    axios
+      .post("https://webhook.site/d2f59c44-4b9a-4f53-a2a6-16ae50bd83c4", {
+        foo: "foo"
+      })
+      .then(res => {
+        console.log(res);
+        callback(true);
+      })
+      .catch(err => {
+        console.log(err);
+        callback(true);
+      });
+  }, [callback]);
+
   return null;
 };
 
