7f62b08f5b7b4465791729cbb197e87610038fc3
Michael Hart <michael@mhart.dev>
2019-12-27 07:04:22 -0500

    Updated endpoint with CORS support

--- src/components/RescueForm.js
+++ src/components/RescueForm.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
-const Popup = ({ message, callback }) => {
+const RescueForm = ({ message, callback }) => {
   axios
-    .post("https://postman-echo.com/post", { foo: "foo" })
+    .post("https://webhook.site/d2f59c44-4b9a-4f53-a2a6-16ae50bd83c4", {
+      foo: "foo"
+    })
     .then(res => {
       console.log(res);
       callback(true);
@@ -14,4 +16,4 @@ const Popup = ({ message, callback }) => {
   return null;
 };
 
-export default Popup;
+export default RescueForm;
